4cce91932c78ee2ec9869861f9a8cee4be6a3a71
Nord <nord.45@icloud.com>
2021-12-05 20:06:56 +0300

    Refactoring code

--- Ventilation/Main_Stantion/Main_Stantion.ino
+++ Ventilation/Main_Stantion/Main_Stantion.ino
@@ -26,7 +26,7 @@ void setup(){
     digitalWrite(in3, LOW);
     digitalWrite(in4, LOW);
     digitalWrite(in5, LOW);
-    Serial.begin(9600);
+    //Serial.begin(9600);
     radio.begin();                                        
     radio.setChannel(5);                                  
     radio.setDataRate     (RF24_250KBPS);                
@@ -38,111 +38,125 @@ void setup(){
 }
 
 void remote_signal(){
-  radio.stopListening();
-  radio.openWritingPipe(111156789);
-  radio.write(&server_data, sizeof(server_data));
-  radio.startListening();
-  delay(500);
+    radio.stopListening();
+    radio.openWritingPipe(111156789);
+    radio.write(&server_data, sizeof(server_data));
+    radio.startListening();
+    //delay(500);
 }
 
 void data_all_control(){
-  while (s1==0 || s2==0){
-    Serial.print("s1="+String(s1));
-    Serial.println("s2="+String(s2));
-    if(radio.available(&pipe)){                                
-      radio.read(&data, sizeof(data));                  
-      if(pipe==1){s1=data[0];}
-      if(pipe==2){s2=data[0];}
-      if(pipe==3){remote_data[0]=data[0];remote_data[1]=data[1];server_data[2]=data[2];}
+    while (s1==0 || s2==0){
+        //Serial.print("s1="+String(s1));
+        //Serial.println("s2="+String(s2));
+        if(radio.available(&pipe)){                                
+            radio.read(&data, sizeof(data));                  
+            if(pipe==1){s1=data[0];}
+            if(pipe==2){s2=data[0];}
+            if(pipe==3){
+                remote_data[0]=data[0];
+                remote_data[1]=data[1];
+                server_data[2]=data[2];
+            }
+        }
+        if (remote_data[0]==1){user_mode();}
+        //remote_signal();
     }
-    delay(500);
-    if (remote_data[0]==1){user_mode();}
-    remote_signal();
-  }
+}
+
+void set_speed(int speed){
+    switch(speed){
+        case 0: // Off vent
+            digitalWrite(in1, LOW);
+            digitalWrite(in2, LOW);
+            digitalWrite(in3, LOW);
+            digitalWrite(in4, LOW);
+            digitalWrite(in5, LOW);
+            break;
+        case 1: // min speed
+            digitalWrite(in2, LOW);
+            digitalWrite(in3, LOW);
+            digitalWrite(in4, LOW);
+            digitalWrite(in5, LOW);
+            delay(500);
+            digitalWrite(in1, HIGH);
+            break;
+        case 2: // 1 speed
+            digitalWrite(in1, LOW);
+            digitalWrite(in3, LOW);
+            digitalWrite(in4, LOW);
+            digitalWrite(in5, LOW);
+            delay(500);
+            digitalWrite(in2, HIGH);
+            break;    
+        case 3: // 2 speed
+            digitalWrite(in1, LOW);
+            digitalWrite(in2, LOW);
+            digitalWrite(in4, LOW);
+            digitalWrite(in5, LOW);
+            delay(500);
+            digitalWrite(in3, HIGH);
+            break;
+        case 4: // 3 speed
+            digitalWrite(in1, LOW);
+            digitalWrite(in2, LOW);
+            digitalWrite(in3, LOW);
+            digitalWrite(in5, LOW);
+            delay(500);
+            digitalWrite(in4, HIGH);
+            break;
+        case 5: // max speed
+            digitalWrite(in1, LOW);
+            digitalWrite(in2, LOW);
+            digitalWrite(in3, LOW);
+            digitalWrite(in4, LOW);
+            delay(500);
+            digitalWrite(in5, HIGH);
+            break;
+    }
 }
 
 void user_mode(){
-  while (remote_data[0]==1){
-    Serial.print("1");
-    if(radio.available(&pipe)){                                
-      radio.read(&data, sizeof(data));                  
-      if(pipe==3){remote_data[0]=data[0];remote_data[1]=data[1];remote_data[2]=data[2];}
-      
-      if (remote_data[1]==0){
-        digitalWrite(in0, HIGH);
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, LOW);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, LOW);
-        digitalWrite(in5, LOW);
-        server_data[1]=0;
-      }
-
-      if (remote_data[1]==5){
-        digitalWrite(in2, LOW);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, LOW);
-        digitalWrite(in5, LOW);
-        digitalWrite(in0, LOW);
-        delay(500);
-        digitalWrite(in1, HIGH);
-        server_data[1]=5;
-      }
-
-      if (remote_data[1]==4){
-        digitalWrite(in1, LOW);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, LOW);
-        digitalWrite(in5, LOW);
-        delay(500);
-        digitalWrite(in0, LOW);
-        digitalWrite(in2, HIGH);
-        server_data[1]=4;
-      }
-
-      if (remote_data[1]==3){
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, LOW);
-        digitalWrite(in4, LOW);
-        digitalWrite(in5, LOW);
-        delay(500);
-        digitalWrite(in0, LOW);
-        digitalWrite(in3, HIGH);
-        server_data[1]=3;
-      }
-
-      if (remote_data[1]==2){
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, LOW);
-        digitalWrite(in3, LOW);
-        digitalWrite(in5, LOW);
-        delay(500);
-        digitalWrite(in0, LOW);
-        digitalWrite(in4, HIGH);
-        server_data[1]=2;
-      }
-
-      if (remote_data[1]==1){
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, LOW);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, LOW);
-        delay(500);
-        digitalWrite(in0, LOW);
-        digitalWrite(in5, HIGH);
-        server_data[1]=1;
-      }
-      server_data[0]=remote_data[0];
-      delay(1000);
-      remote_signal();
-    } 
-  }
-  digitalWrite(in0, HIGH);
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-  digitalWrite(in5, LOW);
+    while (remote_data[0]==1){
+        //Serial.print("1");
+        if(radio.available(&pipe)){                                
+            radio.read(&data, sizeof(data));                  
+            if(pipe==3){
+                remote_data[0]=data[0];
+                remote_data[1]=data[1];
+                remote_data[2]=data[2];
+                server_data[0]=remote_data[0];
+                if (remote_data[1]==0){
+                    set_speed(0);
+                    server_data[1]=0;
+                }
+                if (remote_data[1]==1){
+                    set_speed(1);
+                    server_data[1]=1;
+                }
+                if (remote_data[1]==2){
+                    set_speed(2);
+                    server_data[1]=2;
+                }
+                if (remote_data[1]==3){
+                    set_speed(3);
+                    server_data[1]=3;
+                }
+                if (remote_data[1]==4){
+                    set_speed(4);
+                    server_data[1]=4;
+                }
+                if (remote_data[1]==5){
+                    set_speed(5);
+                    server_data[1]=1;
+                }
+                delay(500);
+                remote_signal();
+            }
+        }
+    }
+    set_speed(0);
+    server_data[1]=0;
 }
 
 void loop(){
@@ -153,67 +167,31 @@ void loop(){
     sv1=int(onv+ksv);
     sv2=int(onv+ksv*2);
     sv3=int(onv+ksv*3);
-
-// Off ventelator
     if (s2<offv){
-      digitalWrite(in0, HIGH);
-      digitalWrite(in1, LOW);
-      digitalWrite(in2, LOW);
-      digitalWrite(in3, LOW);
-      digitalWrite(in4, LOW);
-      digitalWrite(in5, LOW);
-      server_data[1]=0;
+        set_speed(0);
+        server_data[1]=0;
     }
-
-// 1(2) speed ventelator
     if (s2>onv and s2<sv1){
-      digitalWrite(in1, LOW);
-      digitalWrite(in2, LOW);
-      digitalWrite(in3, LOW);
-      delay(500);
-      digitalWrite(in0, LOW);
-      digitalWrite(in4, HIGH);
-      server_data[1]=2;
+        set_speed(2);
+        server_data[1]=2;
     }
-
-// 3 speed ventelstor
-      
     if (s2>sv1 and s2<sv2){
-      server_data[1]=3;
-      digitalWrite(in1, LOW);
-      digitalWrite(in2, LOW);
-      digitalWrite(in4, LOW);
-      delay(500);
-      digitalWrite(in0, LOW);
-      digitalWrite(in3,HIGH);
+        set_speed(3);
+        server_data[1]=3;
     }
-
-// 4 speed ventelator
-      
     if (s2>sv2 and s2<sv3){
-      server_data[1]=4;
-      digitalWrite(in1, LOW);
-      digitalWrite(in3, LOW);
-      digitalWrite(in4, LOW);
-      delay(500);
-      digitalWrite(in0, LOW);
-      digitalWrite(in2,HIGH);
+        set_speed(4);
+        server_data[1]=4;
     }
-
-// max speed ventelator
-      
     if (s2>sv3){
-      server_data[1]=5;
-      digitalWrite(in0, HIGH);
-      digitalWrite(in2, LOW);
-      digitalWrite(in3, LOW);
-      digitalWrite(in4, LOW);
-      delay(500);
-      digitalWrite(in1,HIGH);
-   }
-  Serial.print("Speed server mod:");
-  Serial.println(server_data[1]);   
-  s1=0;
-  s2=0;
-  delay(1000);
+        set_speed(5);
+        server_data[1]=5;
+    }
+    //Serial.print("Speed server mod:");
+    //Serial.println(server_data[1]);   
+    delay(500);
+    remote_signal();
+    s1=0;
+    s2=0;
+    
 }
